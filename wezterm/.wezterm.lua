031bc70af105753cffd0eb7245e5947849bb19ab
joaokhalaf <232938876+joaokhalaf@users.noreply.github.com>
2026-03-23 22:52:44 -0300

    minimal wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,41 +1,20 @@
-local wezterm = require("wezterm")
-local act = wezterm.action
-local config = wezterm.config_builder()
+local wezterm = require 'wezterm'
 
-config.color_scheme = 'Catppuccin Mocha'
+return {
+  default_domain = "WSL:Ubuntu-24.04",
 
-config.front_end = "OpenGL"
-config.prefer_egl = true
-config.max_fps = 144
-config.cursor_blink_rate = 500
-config.term = "xterm-256color"
+  font = wezterm.font("JetBrainsMonoNL Nerd Font"),
+  font_size = 16.0,
 
-config.default_prog = { "wsl.exe", "~" }
+  color_scheme = "Catppuccin Mocha",
 
-config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 16.0
+  window_decorations = "RESIZE",
+  enable_tab_bar = false,
+  window_background_opacity = 0.95,
 
-config.window_background_opacity = 0.65
-config.text_background_opacity = 1.00
-config.window_decorations = "RESIZE"
-config.window_padding = {
-  left = 8,
-  right = 8,
-  top = 4,
-  bottom = 4,
+  initial_cols = 120,
+  initial_rows = 30,
+
+  window_close_confirmation = "NeverPrompt",
+
 }
-config.initial_cols = 100
-config.window_close_confirmation = 'NeverPrompt'
-
-config.enable_scroll_bar = true
-
-config.scrollback_lines = 10000
-
-config.harfbuzz_features = {"calt", "liga", "dlig", "ss01"}
-
-config.pane_focus_follows_mouse = true
-
-config.use_fancy_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
-
-return config
